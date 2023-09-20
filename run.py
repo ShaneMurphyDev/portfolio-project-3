33aca401ecb9ce09c44d8a4ad32804cd9bb8067a
ShaneMurphyDev <>
2023-09-20 11:06:33 +0000

    created main menu frame work

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Name Input
+name = input('Please enter your name > ')
+print(f'Hello {name}. Welcome to my game. I hope you enjoy your time here.')
+
+# Main Menu
+start = input('Start the game, or learn how to play first? Start/Learn > ')
+if start == 'Start' or 'S' or 's':
+    print('Okay. Let us begin.')
