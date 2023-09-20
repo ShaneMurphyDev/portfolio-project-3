ec4bb5c69327a8180bb18df9d2d9f3f73155daff
ShaneMurphyDev <>
2023-09-20 12:15:53 +0000

    fixed indentation error

--- run.py
+++ run.py
@@ -4,18 +4,19 @@ print(f'Hello {name}. Welcome to my game. I hope you enjoy your time here.')
 
 # Main Menu
 while True:
-start = input('Start the game, or learn how to play first? Start/Learn > ')
-if start == 'Start':
-    print(f'Okay, {name}. Let us begin.')
-    break
-elif start == 'Learn':
-    print("")
-    print("The objective of this game is to surive and escape the tundra")
-    print("You will be prompted to make decisions along the way")
-    print("Type your answers and hit enter make a choice")
-    print("Survive untill the end to win the game")
-    print("Make the wrong choice and perish")
-    print("Good luck.")
-    print("")
+    start = input(
+        'Start the game, or learn how to play first? Start/Learn > ')
+    if start == 'Start':
+        print(f'Okay, {name}. Let us begin.')
+        break
+    elif start == 'Learn':
+        print("")
+        print("The objective of this game is to surive and escape the tundra")
+        print("You will be prompted to make decisions along the way")
+        print("Type your answers and hit enter make a choice")
+        print("Survive untill the end to win the game")
+        print("Make the wrong choice and perish")
+        print("Good luck.")
+        print("")
 else:
     print("Invalid choice. Please enter 'Start' or 'Learn'.")
