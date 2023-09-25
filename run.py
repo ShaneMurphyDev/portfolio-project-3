37cd55a771d2f9f194cf4a0e52034adc9c0a33ac
ShaneMurphyDev <>
2023-09-25 09:13:23 +0000

    removed extra file and repositioned code

--- run.py
+++ run.py
@@ -1,5 +1,3 @@
-from function import *
-
 # Name Input
 print("")
 print("#====================#")
@@ -10,6 +8,35 @@ print("#====================#")
 print("")
 name = input('Please enter your name > ')
 print(f'Hello {name}. Welcome to my game. I hope you enjoy your time here.')
+
+
+def intro():
+    print("")
+    print("")
+    print("Your vision is...blurry")
+    print("Ears ringing..")
+    print("Everything feels heavy and tired")
+    print("As your vision clears you can make out some silhouettes")
+    print("An orange flickering light to your right")
+    print("A chill runs through your body")
+    print("There is a mechanical wreck 30 meters away")
+    print("Its a small aircraft, its on fire")
+    print('"Right...the storm..."')
+    print("You look down at yourself and see a badge")
+    print('"Forest Ranger"')
+    print("You start to recall the crash")
+    print("A blur or lights flashing and alarms ringing")
+    print("You were on your way to an isolated research center")
+    print("You did your best to land safely but the tree line..")
+    print("made that impossible")
+    print("You pick yourself up from the ground")
+    print("I need to get to the research station..12 miles east")
+    print("Before you lies 3 paths")
+    print("Continue further up the mountain, shortest but most direct route")
+    print("Enter the cave just ahead, could be good shelter")
+    print("Follow the iced over river")
+    print("")
+    firstChoice = input("Please choose Mountain/Cave/River > ")
 
 
 # Main Menu
@@ -33,3 +60,6 @@ while True:
         print("")
         print("Invalid choice. Please enter 'Start' or 'Learn'.")
         print("")
+
+if firstChoice == 'Mountain':
+    print("")
